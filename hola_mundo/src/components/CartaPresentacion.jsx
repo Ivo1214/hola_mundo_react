0227ad26b1977ac906a41d893980612bdbfe2e72
Ivo1214 <ivo1214@live.com>
2023-07-31 18:17:39 -0300

    Resolucion
    Ejercicios resueltos

---
+++ hola_mundo/src/components/CartaPresentacion.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CartaPresentacion = () => {
+    const nombre = "Ivo";
+    const apellido = "Formica";
+    return (
+        <div>
+            <h1>Hola soy {nombre} {apellido}</h1>
+            <p>Actualmente me encuentro estudiando la carrera Desarrollador Front-End en INOVE y Analista en Informatica en UNSADA. Mi meta a corto/mediano plazo es trabajar 
+                en una empresa desarrolladora de software para pulir mis habilidades. Mi meta a largo plazo es profesionalizarme en Base de Datos.</p>
+        </div>
+    );
+};
+
+export default CartaPresentacion;
